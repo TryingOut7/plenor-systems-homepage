891b842f7af12d6250689c116cad74bd15f7e622
TryingOut7 <marurraghav@gmail.com>
2026-04-06 19:51:44 +0200

    fix: remove orphan migration reference from index

--- migrations/payload/index.ts
+++ migrations/payload/index.ts
@@ -3,7 +3,6 @@ import * as migration_20260405_000001 from './20260405_000001';
 import * as migration_20260406_000000 from './20260406_000000';
 import * as migration_20260406_000001 from './20260406_000001';
 import * as migration_20260406_000002 from './20260406_000002';
-import * as migration_20260406_171118_final_sync_pre_deploy from './20260406_171118_final_sync_pre_deploy';
 
 export const migrations = [
   {
@@ -30,10 +29,5 @@ export const migrations = [
     up: migration_20260406_000002.up,
     down: migration_20260406_000002.down,
     name: '20260406_000002',
-  },
-  {
-    up: migration_20260406_171118_final_sync_pre_deploy.up,
-    down: migration_20260406_171118_final_sync_pre_deploy.down,
-    name: '20260406_171118_final_sync_pre_deploy'
   },
 ];
