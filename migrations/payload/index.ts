f1d72a2557e1219c74fe72c28f4d2855ff6e3cc0
TryingOut7 <marurraghav@gmail.com>
2026-04-08 21:38:37 +0200

    chore(migrations): register org_feed_section migration in index and add json snapshot

--- migrations/payload/index.ts
+++ migrations/payload/index.ts
@@ -6,6 +6,7 @@ import * as migration_20260406_000002 from './20260406_000002';
 import * as migration_20260406_000003 from './20260406_000003';
 import * as migration_20260407_000000 from './20260407_000000';
 import * as migration_20260408_075601 from './20260408_075601';
+import * as migration_20260408_193202_org_feed_section from './20260408_193202_org_feed_section';
 
 export const migrations = [
   {
@@ -46,6 +47,11 @@ export const migrations = [
   {
     up: migration_20260408_075601.up,
     down: migration_20260408_075601.down,
-    name: '20260408_075601'
+    name: '20260408_075601',
+  },
+  {
+    up: migration_20260408_193202_org_feed_section.up,
+    down: migration_20260408_193202_org_feed_section.down,
+    name: '20260408_193202_org_feed_section'
   },
 ];
